a8e9c6ee83810acfbdfb233a9fd98b2fbbb361fc
rb <842549829@qq.com>
2025-04-09 17:56:27 +0800

    feat:机构字段扩展

--- modules/net/src/Heal.Net.EntityFrameworkCore/EntityFrameworkCore/HealNetEfCoreEntityExtensionMappings.cs
+++ modules/net/src/Heal.Net.EntityFrameworkCore/EntityFrameworkCore/HealNetEfCoreEntityExtensionMappings.cs
@@ -51,6 +51,7 @@ public static class HealNetEfCoreEntityExtensionMappings
              * https://docs.abp.io/en/abp/latest/Customizing-Application-Modules-Extending-Entities
              */
 
+            // 用户扩展信息
             ObjectExtensionManager.Instance
                 .MapEfCoreProperty<IdentityUser, string>(
                     IdentityUserExtensionConsts.OpenId,
@@ -74,6 +75,7 @@ public static class HealNetEfCoreEntityExtensionMappings
                         propertyBuilder.IsRequired();
                     });
 
+            // 角色扩展信息
             ObjectExtensionManager
                 .Instance
                 .MapEfCoreProperty<IdentityRole, int>(IdentityRoleExtensionConsts.DataPermission,
@@ -88,6 +90,142 @@ public static class HealNetEfCoreEntityExtensionMappings
                         propertyBuilder.HasMaxLength(IdentityRoleExtensionConsts.MaxCustomDataPermissionLength);
                         propertyBuilder.HasDefaultValue(string.Empty);
                         propertyBuilder.IsRequired();
+                    });
+
+            // 机构扩展信息
+            ObjectExtensionManager
+                .Instance
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.Director,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("院长");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxDirectorLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, int?>(OrganizationUnitExtensionConsts.Level,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("医院等级");
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.Phone,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("联系电话");
+                    })
+                .MapEfCoreProperty<OrganizationUnit, DateTime?>(OrganizationUnitExtensionConsts.EstablishmentDate,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("成立时间");
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.Email,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("邮箱");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxEmailLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.WebsiteUrl,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("官方网站");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxWebsiteUrlLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.Address,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("地址");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxAddressLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.PostalCode,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("邮政编码");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxPostalCodeLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.ServiceHotline,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("服务热线");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxServiceHotlineLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.Introduction,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("医院简介");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxIntroductionLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.TrafficGuide,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("交通指南");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxTrafficGuideLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.ParkingInformation,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("停车信息");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxParkingInformationLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.Describe,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("备注描述");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxDescribeLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, decimal?>(OrganizationUnitExtensionConsts.Latitude,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("纬度");
+                    })
+                .MapEfCoreProperty<OrganizationUnit, decimal?>(OrganizationUnitExtensionConsts.Longitude,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("经度");
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.CoverImage,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("封面图片");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxCoverImageLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, string?>(OrganizationUnitExtensionConsts.Facilities,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("医院设施");
+                        propertyBuilder.HasMaxLength(OrganizationUnitExtensionConsts.MaxFacilitiesLength);
+                    })
+                .MapEfCoreProperty<OrganizationUnit, DateTime?>(OrganizationUnitExtensionConsts.OperatingHours,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("营业时间");
+                    })
+                .MapEfCoreProperty<OrganizationUnit, bool?>(OrganizationUnitExtensionConsts.IsEmergencyServices,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("是否提供急诊服务");
+                    })
+                .MapEfCoreProperty<OrganizationUnit, bool?>(OrganizationUnitExtensionConsts.IsInsuranceAccepted,
+                    (_, propertyBuilder) =>
+                    {
+                        propertyBuilder.IsRequired(false);
+                        propertyBuilder.HasComment("是否接受医保");
                     });
         });
     }
---
+++ modules/net/src/Heal.Net.EntityFrameworkCore/Migrations/Script/20250409095310_init3.sql
@@ -0,0 +1,298 @@
+﻿START TRANSACTION;
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `Address` varchar(256) CHARACTER SET utf8mb4 NULL COMMENT '地址';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `CoverImage` varchar(1024) CHARACTER SET utf8mb4 NULL COMMENT '封面图片';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `Describe` varchar(512) CHARACTER SET utf8mb4 NULL COMMENT '备注描述';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `Director` varchar(64) CHARACTER SET utf8mb4 NULL COMMENT '院长';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `Email` varchar(64) CHARACTER SET utf8mb4 NULL COMMENT '邮箱';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `EstablishmentDate` datetime(6) NULL COMMENT '成立时间';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `Facilities` varchar(1024) CHARACTER SET utf8mb4 NULL COMMENT '医院设施';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `Introduction` varchar(4096) CHARACTER SET utf8mb4 NULL COMMENT '医院简介';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `IsEmergencyServices` tinyint(1) NULL COMMENT '是否提供急诊服务';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `IsInsuranceAccepted` tinyint(1) NULL COMMENT '是否接受医保';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `Latitude` decimal(65,30) NULL COMMENT '纬度';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `Level` int NULL COMMENT '医院等级';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `Longitude` decimal(65,30) NULL COMMENT '经度';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `OperatingHours` datetime(6) NULL COMMENT '营业时间';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `ParkingInformation` varchar(512) CHARACTER SET utf8mb4 NULL COMMENT '停车信息';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `Phone` longtext CHARACTER SET utf8mb4 NULL COMMENT '联系电话';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `PostalCode` varchar(16) CHARACTER SET utf8mb4 NULL COMMENT '邮政编码';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `ServiceHotline` varchar(32) CHARACTER SET utf8mb4 NULL COMMENT '服务热线';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `TrafficGuide` varchar(512) CHARACTER SET utf8mb4 NULL COMMENT '交通指南';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    ALTER TABLE `AbpOrganizationUnits` ADD `WebsiteUrl` varchar(128) CHARACTER SET utf8mb4 NULL COMMENT '官方网站';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250409095310_init3') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20250409095310_init3', '9.0.0');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+COMMIT;
+
