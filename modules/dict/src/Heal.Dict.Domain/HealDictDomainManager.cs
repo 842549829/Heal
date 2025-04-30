c54d10e38dfbe0080c6df54e8b39c7546fe2035c
rb <842549829@qq.com>
2025-04-30 17:47:28 +0800

    feat:添加字典模块

---
+++ modules/dict/src/Heal.Dict.Domain/HealDictDomainManager.cs
@@ -0,0 +1,8 @@
+﻿using Heal.Domain;
+
+namespace Heal.Dict.Domain;
+
+/// <summary>
+/// 领域服务基类
+/// </summary>
+public abstract class HealDictDomainManager : HealDomainManager;
---
+++ modules/dict/src/Heal.Dict.Domain/IHealDictDomainManager.cs
@@ -0,0 +1,8 @@
+﻿using Heal.Domain;
+
+namespace Heal.Dict.Domain;
+
+/// <summary>
+/// IHealCoreDomainManager
+/// </summary>
+public interface IHealDictDomainManager : IHealDomainManager;
