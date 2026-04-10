786350f140528cf60ceea37e067684a06f84a4d5
yade <842549829@qq.com>
2026-04-10 11:34:47 +0800

    docs:数据库迁移

---
+++ modules/net/src/Heal.Net.EntityFrameworkCore/Migrations/20260410032502_Update1020.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Heal.Net.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update1020 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Leaved",
+                table: "AbpUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PropertyTypeFullName",
+                table: "AbpEntityPropertyChanges",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                comment: "属性全称",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64,
+                oldComment: "属性全称");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityTypeFullName",
+                table: "AbpEntityChanges",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                comment: "审计实体全称",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128,
+                oldComment: "审计实体全称");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Leaved",
+                table: "AbpUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PropertyTypeFullName",
+                table: "AbpEntityPropertyChanges",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                comment: "属性全称",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512,
+                oldComment: "属性全称");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityTypeFullName",
+                table: "AbpEntityChanges",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                comment: "审计实体全称",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512,
+                oldComment: "审计实体全称");
+        }
+    }
+}
---
+++ modules/net/src/Heal.Net.EntityFrameworkCore/Migrations/Script/20260410032502_Update1020.sql
@@ -0,0 +1,51 @@
+﻿BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260410032502_Update1020'
+)
+BEGIN
+    ALTER TABLE [AbpUsers] ADD [Leaved] bit NOT NULL DEFAULT CAST(0 AS bit);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260410032502_Update1020'
+)
+BEGIN
+    DECLARE @var nvarchar(max);
+    SELECT @var = QUOTENAME([d].[name])
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[AbpEntityPropertyChanges]') AND [c].[name] = N'PropertyTypeFullName');
+    IF @var IS NOT NULL EXEC(N'ALTER TABLE [AbpEntityPropertyChanges] DROP CONSTRAINT ' + @var + ';');
+    ALTER TABLE [AbpEntityPropertyChanges] ALTER COLUMN [PropertyTypeFullName] nvarchar(512) NOT NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260410032502_Update1020'
+)
+BEGIN
+    DROP INDEX [IX_AbpEntityChanges_TenantId_EntityTypeFullName_EntityId] ON [AbpEntityChanges];
+    DECLARE @var1 nvarchar(max);
+    SELECT @var1 = QUOTENAME([d].[name])
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[AbpEntityChanges]') AND [c].[name] = N'EntityTypeFullName');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [AbpEntityChanges] DROP CONSTRAINT ' + @var1 + ';');
+    ALTER TABLE [AbpEntityChanges] ALTER COLUMN [EntityTypeFullName] nvarchar(512) NOT NULL;
+    CREATE INDEX [IX_AbpEntityChanges_TenantId_EntityTypeFullName_EntityId] ON [AbpEntityChanges] ([TenantId], [EntityTypeFullName], [EntityId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260410032502_Update1020'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260410032502_Update1020', N'10.0.5');
+END;
+
+COMMIT;
+GO
+
